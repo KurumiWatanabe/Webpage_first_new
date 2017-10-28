8e2255cca60ce8874edb2fcafb78c647876b4d85
KurumiWatanabe <internmarokaan1217@gmail.com>
2017-10-29 00:22:00 +0900

    import_first

---
+++ login.js
@@ -0,0 +1,14 @@
+function hyoji1(num)
+{
+ if (num == 0)
+ {
+   document.getElementById("hoge1").style.display="block";
+   document.getElementById("load-more-content").style.display="none";
+ }
+
+ /*else if (num == 1)
+ {
+   document.getElementById("hoge2").style.display="none";
+ }
+*/
+}
